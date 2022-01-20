d2f508c11c77c04d3079763c687c6b348f3fc386
GlendonNotGlen <glendonng38@gmail.com>
2022-01-20 17:07:20 +0800

    add opening and closing message

---
+++ Duke.java
@@ -0,0 +1,12 @@
+public class Duke {
+    public static void main(String[] args) {
+        String openingMsg = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        String closingMsg = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(openingMsg);
+        System.out.println(closingMsg);
+    }
+}
